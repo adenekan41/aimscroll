850b82b338e34dfa8d345ed0825e0d9822437fe6
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2022-02-09 02:24:51 +0100

    🚀(chore): prune unconditional hooks call

--- src/use-aim-scroll.ts
+++ src/use-aim-scroll.ts
@@ -11,8 +11,6 @@ const useAimScroll = (
   scrollStart: number,
   scrollEnd?: number
 ): [boolean?, React.Dispatch<React.SetStateAction<boolean>>?] => {
-  if (!isBrowser()) return [undefined, undefined];
-
   // Check if we have a scrollStart and its a number
   if (!scrollStart || typeof scrollStart !== 'number')
     throw new Error('aimScroll needs to have a start value');
@@ -36,11 +34,13 @@ const useAimScroll = (
   }, []);
 
   useWillMount(() => {
-    window.addEventListener('scroll', scrollWindow);
+    if (isBrowser()) {
+      window.addEventListener('scroll', scrollWindow);
 
-    return function cleanupListener() {
-      window.removeEventListener('scroll', scrollWindow);
-    };
+      return function cleanupListener() {
+        window.removeEventListener('scroll', scrollWindow);
+      };
+    }
   });
 
   return [isScroll, setScroll];
